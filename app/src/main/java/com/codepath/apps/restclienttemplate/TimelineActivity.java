d04b6c4ed27ccd14bbd124986ff8cc1b24032630
kat-kime <katckime@gmail.com>
2020-10-09 23:25:01 -0700

    first commit

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -0,0 +1,98 @@
+package com.codepath.apps.restclienttemplate;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.codepath.apps.restclienttemplate.models.Tweet;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Headers;
+
+public class TimelineActivity extends AppCompatActivity {
+    public static final String TAG = "TimelineActivity";
+    TwitterClient client;
+    RecyclerView rvTweets;
+    List<Tweet> tweets;
+    TweetAdapter adapter;
+    SwipeRefreshLayout swipeContainer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timeline);
+
+        client = TwitterApplication.getRestClient(this);
+
+        swipeContainer = findViewById(R.id.swipeContainer);
+        // Configure the refreshing colors
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+
+
+        // Adding swipe listener
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                Log.d(TAG, "Fetching new data");
+                populateHomeTimeline();
+            }
+        });
+
+        // Find the recycler view
+        rvTweets = findViewById(R.id.rvTweets);
+
+        // Initialize the list of tweets and adapter
+        tweets = new ArrayList<>();
+        adapter = new TweetAdapter(this, tweets);
+
+        // REcycler view setup; layout manager and adapter
+        rvTweets.setAdapter(adapter);
+        rvTweets.setLayoutManager(new LinearLayoutManager(this));
+
+        populateHomeTimeline();
+    }
+
+    private void populateHomeTimeline() {
+        client.getHomeTimeline(new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                Log.i(TAG, "On Success\n" + json.toString());
+
+                JSONArray jsonArray = json.jsonArray;
+                try {
+                    adapter.clear();
+                    adapter.addAll(Tweet.fromJsonArray(jsonArray));
+                    swipeContainer.setRefreshing(false);
+                } catch (JSONException e) {
+                    Log.d(TAG, "Error when parsing json");
+                    e.printStackTrace();
+                }
+
+                // Create a tweet - i feel like "add all" comes into play
+                // Somehow get json array
+                // Use tweet method fromJsonArray to create a list of tweets
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                Log.i(TAG, "On Failure", throwable);
+
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetAdapter.java
@@ -0,0 +1,96 @@
+package com.codepath.apps.restclienttemplate;
+
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.codepath.apps.restclienttemplate.models.Tweet;
+
+import java.util.List;
+
+// Create Model
+// Create ViewHolder
+// Bind data
+public class TweetAdapter extends RecyclerView.Adapter<TweetAdapter.ViewHolder>{
+    Context context;
+    List<Tweet> tweets;
+
+    public TweetAdapter(Context context, List<Tweet> tweets) {
+        this.context = context;
+        this.tweets = tweets;
+    }
+
+
+    // Inflate the layout
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Log.d("TweetAdapter", "onCreateViewHolder");
+        View tweetView = LayoutInflater.from(context).inflate(R.layout.item_tweet, parent, false);
+        return new ViewHolder(tweetView);
+    }
+
+    // Inflate layout for each row
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Log.d("TweetAdapter", "onBindViewHolder");
+        
+        // Get tweet at position
+        Tweet tweet = tweets.get(position);
+        
+        // Bind the tweet
+        holder.bind(tweet);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return tweets.size();
+    }
+
+    // Clean all elements in the recycler view
+    public void clear() {
+        tweets.clear();
+        notifyDataSetChanged();
+    }
+
+    // Add list of items to our data set
+    public void addAll(List<Tweet> tweetList) {
+        tweets.addAll(tweetList);
+        notifyDataSetChanged();
+    }
+
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        RelativeLayout container;
+        ImageView profile;
+        TextView screenName;
+        TextView body;
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            profile = itemView.findViewById(R.id.ivProfile);
+            screenName = itemView.findViewById(R.id.tvScreenName);
+            body = itemView.findViewById(R.id.tvBody);
+        }
+
+        // Setting the data to the views
+        public void bind(Tweet tweet) {
+            screenName.setText(tweet.user.getScreenName());
+            body.setText(tweet.getBody());
+            Glide.with(context).load(tweet.getUser().getProfileImageUrl()).into(profile);
+        }
+    }
+}
